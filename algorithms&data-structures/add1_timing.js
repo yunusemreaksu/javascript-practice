4eb60bab6df3020374ae6c38014e21f98b1f6fd9
Yunus Emre Aksu <emre_aksu@live.com>
2022-11-27 11:37:28 +0300

    Timing our code

---
+++ algorithms&data-structures/add1_timing.js
@@ -0,0 +1,12 @@
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds.`);
