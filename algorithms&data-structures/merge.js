8199e826ad008552277b6158416028e675f61f99
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-04 10:49:40 +0300

    Added merge sort algorithms

---
+++ algorithms&data-structures/merge.js
@@ -0,0 +1,26 @@
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return console.log(result);
+}
+
+merge([100], [1, 2, 3, 5, 6]);
