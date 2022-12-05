1e9b02ba39af1c586505261a3cfdda06de16c4db
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-05 14:30:02 +0300

    Singly linked list pop solution

--- algorithms&data-structures/singly_linked_list.js
+++ algorithms&data-structures/singly_linked_list.js
@@ -23,10 +23,30 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push("hi");
 list.push("user");
 list.push("admin");
-console.log(list.head);
+list.pop();
+list.pop();
+list.pop();
+console.log(list);
