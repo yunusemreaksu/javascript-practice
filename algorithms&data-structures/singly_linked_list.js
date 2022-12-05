fd4629c87d56158f41bd1f231ca8e28d452b0231
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-05 12:26:44 +0300

    Singly linked list push solution

--- algorithms&data-structures/singly_linked_list.js
+++ algorithms&data-structures/singly_linked_list.js
@@ -11,4 +11,22 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+let list = new SinglyLinkedList();
+list.push("hi");
+list.push("user");
+list.push("admin");
+console.log(list.head);
