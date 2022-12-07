8e571abc3585d50ef3f4f521b11fee22797e05dd
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-07 10:21:55 +0300

    Doubly linked list push solution

--- algorithms&data-structures/doubly_linked_list.js
+++ algorithms&data-structures/doubly_linked_list.js
@@ -12,4 +12,24 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList();
+list.push(10);
+list.push(12);
+list.push(14);
+
+console.log(list);
