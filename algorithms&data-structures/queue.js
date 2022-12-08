e135659db19a466a390a276d8f09146902bfb2ce
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-08 10:27:36 +0300

    Queue enqueue solution

--- algorithms&data-structures/queue.js
+++ algorithms&data-structures/queue.js
@@ -11,4 +11,22 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
 }
+
+let queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(12);
+queue.enqueue(14);
+
+console.log(queue);
