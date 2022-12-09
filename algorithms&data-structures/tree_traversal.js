3e320d7c70271b1cad9ed4e4fecf495ee9b693ea
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-09 10:03:36 +0300

    Breadth first search solution

--- algorithms&data-structures/tree_traversal.js
+++ algorithms&data-structures/tree_traversal.js
@@ -66,4 +66,32 @@ class BinarySearchTree {
     }
     return false;
   }
+  breadthFirstSearch() {
+    let data = [];
+    let queue = [];
+    let node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      // data.push(node);
+      data.push(node.value);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    return data;
+  }
 }
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+
+console.log(tree.breadthFirstSearch());
