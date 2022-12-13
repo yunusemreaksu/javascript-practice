926955b6cb9220945ac52372a76e133d05c2d950
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-13 21:43:29 +0300

    Graph add edge solution

--- algorithms&data-structures/graph.js
+++ algorithms&data-structures/graph.js
@@ -5,9 +5,16 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
 }
 
 const graph = new Graph();
 graph.addVertex("Tokyo");
+graph.addVertex("Dallas");
+graph.addVertex("Aspen");
+graph.addEdge("Dallas", "Aspen");
 
 console.log(graph);
