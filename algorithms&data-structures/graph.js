fcf0a9efd423bbe80810f55d97d37b2df011a541
Yunus Emre Aksu <emre_aksu@live.com>
2022-12-13 21:56:13 +0300

    Graph remove edge solution

--- algorithms&data-structures/graph.js
+++ algorithms&data-structures/graph.js
@@ -9,6 +9,14 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
 }
 
 const graph = new Graph();
@@ -16,5 +24,9 @@ graph.addVertex("Tokyo");
 graph.addVertex("Dallas");
 graph.addVertex("Aspen");
 graph.addEdge("Dallas", "Aspen");
+graph.addEdge("Dallas", "Tokyo");
 
+console.log(graph);
+
+graph.removeEdge("Dallas", "Aspen");
 console.log(graph);
